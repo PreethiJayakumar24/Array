1068debd881f557eb83b86089d91ee57e6b2da28
preesubs <varshinijayakumar16&gmail.com>
2024-12-23 10:40:59 +0530

    First Commit

---
+++ findNo.java
@@ -0,0 +1,44 @@
+package Array;
+
+import java.util.Scanner;
+
+public class findNo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner Sc = new Scanner(System.in);
+		System.out.println("Enter the No:");
+		int total = Sc.nextInt();
+		int [] n = new int[total];
+		
+	//	int i =0;
+		for(int i= 0;i<total;i++) {
+		System.out.println("Enter the Array No:");
+		n[i] = Sc.nextInt();		
+		}
+		
+		System.out.println("Enter the Find No: ");
+		int FindNo = Sc.nextInt();
+		
+		boolean Found = false;
+	//	for(int i=0;i<total;i++) 
+			int i = 0;
+			while(i<total) {
+			if(n[i]==FindNo) {
+			//	System.out.println(n[i]);
+			System.out.println("Placed element" + (i + 1));
+			Found = true;
+			break;
+			}
+			i++;
+			}
+			
+		if(!Found) {
+		System.out.println("The number is not here");
+		}
+			
+		
+		Sc.close();
+	}
+
+}
