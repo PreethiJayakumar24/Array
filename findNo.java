0ce90f4fcc5601e17e776b4b92e2a044e7d4d269
preesubs <varshinijayakumar16&gmail.com>
2024-12-23 12:01:55 +0530

    Second commit

--- findNo.java
+++ findNo.java
@@ -7,17 +7,17 @@ public class findNo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner Sc = new Scanner(System.in);
-		System.out.println("Enter the No:");
+		System.out.println("Enter the Number:");
 		int total = Sc.nextInt();
 		int [] n = new int[total];
 		
 	//	int i =0;
 		for(int i= 0;i<total;i++) {
-		System.out.println("Enter the Array No:");
+		System.out.println("Enter the Array Number:");
 		n[i] = Sc.nextInt();		
 		}
 		
-		System.out.println("Enter the Find No: ");
+		System.out.println("Enter the Find Number: ");
 		int FindNo = Sc.nextInt();
 		
 		boolean Found = false;
